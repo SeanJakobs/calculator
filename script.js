d23bc0246805b8e1644b6a706a434bdc97113931
Sean Jakobs <seanjakobs@gmail.com>
2021-02-24 20:55:20 -0700

    added functions for updating display and event listeners to number buttons as well as updated HTML with classes and IDs for number buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let displayValue = 0;
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -17,3 +19,19 @@ function divide(num1, num2) {
 function operate(operator, num1, num2){
     return operator(num1, num2);
 }
+
+function test(){
+    alert("working")
+}
+
+function updateDisplay (num){
+    let displayValue = num;
+    console.log(displayValue);
+}
+
+
+const numberButtons = document.getElementsByClassName("num");
+Array.from(numberButtons).forEach(num => {
+    num.addEventListener('click', test);
+});
+
